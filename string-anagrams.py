ed5fda99157c0d53b2fec3f5af04979475fdcf81
Akshat Songara <songaraakshat71@gmail.com>
2026-08-11 08:40:32 +0530

    String-Anagram

---
+++ string-anagrams.py
@@ -0,0 +1,22 @@
+string1 = input("Enter First String : ")
+
+string2 = input("Enter Second String : ")
+
+if len(string1) != len(string2):
+
+    print("Strings are  not anagrams.")
+
+else:
+
+    for ch in string1:
+
+        if string1.count(ch) != string2.count(ch):
+
+            print("Strings are not anagrams.")
+
+            break
+
+    else:
+
+        print("Strings are anagrams.")
+

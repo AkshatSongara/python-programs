ffd0dce1edd473fee63246a3e08b01f447da05e9
Akshat Songara <songaraakshat71@gmail.com>
2026-08-20 07:33:35 +0530

    Positive-Negetive-Zero

---
+++ positive-negetive-zero.py
@@ -0,0 +1,13 @@
+num = int(input("Enter the number: "))
+
+if num == 0:
+
+    print("Given number is zero.")
+
+elif num < 0:
+
+    print("Given number is negative.")
+
+else:
+
+    print("Given number is positive.")

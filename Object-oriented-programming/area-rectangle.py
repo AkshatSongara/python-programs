7efbd2715957b9fa44e5f3734bbfb0c682dbe68f
Akshat Songara <songaraakshat71@gmail.com>
2026-08-24 08:03:31 +0530

    Area-of-Rectangle

--- Object-oriented-programming/area-rectangle.py
+++ Object-oriented-programming/area-rectangle.py
@@ -1,4 +1,4 @@
-class Reactangle:
+class Rectangle:
 
     length = 0
     width = 0
@@ -12,17 +12,17 @@ class Reactangle:
         return 2 * (self.length + self.width)
 
 
-r1 = Reactangle()
-r2 = Reactangle()
+r1 = Rectangle()
+r2 = Rectangle()
 
 r1.length = 15
 r1.width = 20
 
-print("Area of reactangle:", r1.area())
+print("Area of rectangle:", r1.area())
 print("Area of perimeter:", r1.perimeter())
 
 r2.length = 25
 r2.width = 5
 
-print("Area of reactangle:", r2.area())
+print("Area of rectangle:", r2.area())
 print("Area of perimeter:", r2.perimeter())

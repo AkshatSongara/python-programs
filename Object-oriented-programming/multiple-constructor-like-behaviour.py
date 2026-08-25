1fcd34047204d5167822c7c7393f4134a8560f31
Akshat Songara <songaraakshat71@gmail.com>
2026-08-25 07:59:57 +0530

    constructor-Overloading-Like-Behaviour

---
+++ Object-oriented-programming/multiple-constructor-like-behaviour.py
@@ -0,0 +1,30 @@
+class Employee:
+
+    def __init__(self, name = "Unknown", salary = 0, department = "Not assigned"):
+
+        self.name = name
+        self.salary = salary
+        self.department = department
+
+    def print(self):
+
+        print("Employee name:", self.name)
+        print("Employee salary:", self.salary)
+        print("Employee department:", self.department)
+
+
+e1 = Employee()
+e2 = Employee("Kunal")
+e3 = Employee("Santosh", 10000)
+e4 = Employee("Ramesh", 15000, "Technical")
+
+e1.print()
+print()
+
+e2.print()
+print()
+
+e3.print()
+print()
+
+e4.print()

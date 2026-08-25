4e772766ef51c8fc1a872d6638468872862b5622
Akshat Songara <songaraakshat71@gmail.com>
2026-08-25 07:46:29 +0530

    Parameterized-Constructor

---
+++ Object-oriented-programming/parameterized-constructor.py
@@ -0,0 +1,17 @@
+class Student:
+
+    def __init__(self, name, age):
+
+        self.name = name
+        self.age = age
+
+s1 = Student("Manoj", 22)
+s2 = Student("Himesh", 24)
+
+print("Student Name:", s1.name)
+print("Student Name:", s1.age)
+
+print()
+
+print("Student Name:", s2.name)
+print("Student Name:", s2.age)

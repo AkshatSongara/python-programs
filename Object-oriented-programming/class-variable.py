bf192c25e073c39cb028821eb8fdc3bafec58555
Akshat Songara <songaraakshat71@gmail.com>
2026-08-26 07:32:59 +0530

    Class-Variable

---
+++ Object-oriented-programming/class-variable.py
@@ -0,0 +1,32 @@
+class Employee:
+
+    company = "TechCorp"
+
+    def __init__(self, name, salary):
+
+        self.name = name
+        self.salary = salary
+
+    def show(self):
+
+        print("Employee Name:", self.name)
+        print("Employee salary:", self.salary)
+        print("Employee Company:", self.company)
+        # print("Employee Company", Employee.company)
+
+e1 = Employee("Kishore", 15000)
+e2 = Employee("Vinod", 20000)
+
+e1.show()
+print()
+
+e2.show()
+print()
+
+Employee.company = "Google"
+
+e1.show()
+print()
+
+e2.show()
+print()

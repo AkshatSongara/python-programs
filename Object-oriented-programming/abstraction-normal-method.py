d0e3f4c829030ff04f8eebf916104362e0837da2
Akshat Songara <songaraakshat71@gmail.com>
2026-09-02 19:52:20 +0530

    Abstraction With Abstract-Method and  Normal-Method

---
+++ Object-oriented-programming/abstraction-normal-method.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+
+    @abstractmethod
+    def sound(self):
+        pass
+
+    def eat(self):
+        print("Animal is eating")
+
+class Dog(Animal):
+
+    def sound(self):
+        print("Dog Barks")
+
+dog = Dog()
+
+dog.eat()
+dog.sound()

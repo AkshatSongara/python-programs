46ecb4d9b009188d3a111c1c3576ec649a161193
Akshat Songara <songaraakshat71@gmail.com>
2026-09-07 10:07:46 +0530

    Library Management Project

--- Object-oriented-programming/Library-Management.py
+++ Object-oriented-programming/Library-Management.py
@@ -6,8 +6,8 @@ class Book:
         self.author = author
 
     def display(self):
-        # status = "Available" if self.is_available else "Issued"
-        print(f"{self.book_id} | {self.title} | {self.title} | {self.author}")
+        status = "Available" if self.is_available else "Issued"
+        print(f"{self.book_id} | {self.title} | {self.title} | {self.author} | {status}")
 
 
 class Student:
@@ -31,6 +31,29 @@ class Librarian:
         print(f"Librarian Id: ", {self.librarian_id})
         print(f"Name: ", {self.name})
 
+
+class Transaction:
+
+    def __init__(self, book, student):
+        self.book = book
+        self.student = student
+
+    def issued_book(self):
+        if self.book.is_available:
+            self.book.is_available = False
+            print(f"Book '{self.book.title}' issued to {self.student.name}.")
+        else:
+            print("Sorry, this book is already issued.")
+
+    def return_book(self):
+        if not self.book.is_available:
+            self.book.is_available = True
+            print(f"Book '{self.book.title}' returned by {self.student.name}.")
+        else:
+            print("This book was not issued")
+
+
+
 book1 = Book(101, "Python Programming", "John Smith")
 book2 = Book(102, "Data Structures", "Robert Brown")
 
@@ -42,3 +65,7 @@ student1.display()
 
 librarian = Librarian(501, "Priya")
 librarian.display()
+
+transaction = Transaction(book1, student1)
+transaction.issued_book()
+transaction.return_book()
